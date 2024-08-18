aaf79db9787f5334c01011e038a544b64a97094f
Leonardo Da Vinci <84631921+leanardo7994@users.noreply.github.com>
2024-08-18 11:34:52 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -88,8 +88,8 @@ async def start_command(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("👤 About Me", callback_data = "about"),
-                    InlineKeyboardButton("🔒 Close", callback_data = "close")
+                    InlineKeyboardButton("👤 ABOUT ME", callback_data = "about"),
+                    InlineKeyboardButton("🔒 CLOSE", callback_data = "close")
                 ]
             ]
         )
